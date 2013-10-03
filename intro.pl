5fb2ccda448272a5cebaedbcbc6199bacb58c48b
Toon Nolten <toon.nolten@student.kuleuven.be>
2013-10-03 16:32:14 +0200

    eerste oefening 1.1

---
+++ intro.pl
@@ -0,0 +1,11 @@
+% intro.pl
+
+vader(anton, bart).
+vader(anton, daan).
+vader(anton, elisa).
+vader(fabian, anton).
+
+moeder(celine, bart).
+moeder(celine, daan).
+moeder(celine, gerda).
+moeder(gerda, hendrik).
