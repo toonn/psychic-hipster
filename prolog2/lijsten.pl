3c4f4776d70754b0f3410845303c682255661473
Toon Nolten <toon.nolten@student.kuleuven.be>
2013-10-10 17:34:55 +0200

    vector_som en zoekop

--- prolog2/lijsten.pl
+++ prolog2/lijsten.pl
@@ -15,6 +15,16 @@ laatste([_Head|Rest], Last) :-
     laatste(Rest, Last).
 
 naast([A|[B|_Rest]], A, B).
-naast([A|[B|_Rest]], B, A).
+%naast([A|[B|_Rest]], B, A).
 naast([_X|Y], A, B) :-
     naast(Y, A, B).
+
+vector_som([], [], []).
+vector_som([A|B],[C|D],[S|Tail]) :-
+    S is A + C,
+    vector_som(B, D, Tail).
+
+zoekop([paar(Naam, Score)|_], Naam, Score). 
+zoekop([_|Y], Naam, Score) :-
+    zoekop(Y, Naam, Score).
+
