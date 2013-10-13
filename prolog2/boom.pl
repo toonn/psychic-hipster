5c359feb011159d0e1a9e4b5388d3238c51925eb
Toon Nolten <toon.nolten@student.kuleuven.be>
2013-10-13 13:06:36 +0200

    gebalanceerde bomen extra

--- prolog2/boom.pl
+++ prolog2/boom.pl
@@ -19,4 +19,34 @@ voeg_in(knoop(L,Wo,R), W, knoop(Ln, Wo, R)) :-
 voeg_in(knoop(L,Wo,R), W, knoop(L, Wo, Rn)) :-
     voeg_in(R, W, Rn),
     gebalanceerd(knoop(L, Wo, Rn)).
-    
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+depth(leeg, -1). 
+depth(dk(_,_,_,Depth), Depth).
+
+geb(leeg).
+geb(dk(L,_,R,_)) :-
+    depth(L,Dl),
+    depth(R,Dr),
+    Verschil is Dl - Dr,
+    abs(Verschil) =< 1.
+
+voeg_in_d(leeg, W, dk(leeg, W, leeg, 0)).
+voeg_in_d(dk(L,Wo,R,Depth), W, dk(Ln,Wo,R,Depth)) :-
+    depth(L,Dl),
+    depth(R,Dr),
+    Dl < Dr,
+    voeg_in_d(L,W,Ln).
+voeg_in_d(dk(L,Wo,R,_), W, dk(Ln,Wo,R,DepthN)) :-
+    depth(L,Dl),
+    depth(R,Dr),
+    Dr == Dl,
+    voeg_in_d(L,W,Ln),
+    depth(Ln,Dn),
+    DepthN is Dn + 1.
+voeg_in_d(dk(L,Wo,R,Depth), W, dk(L,Wo,Rn,Depth)) :-
+    depth(L,Dl),
+    depth(R,Dr),
+    Dl > Dr,
+    voeg_in_d(R,W,Rn).
