3cb9a891c15ef8a6f99a8843eafdddf0d4c85bf7
Toon Nolten <toon.nolten@student.kuleuven.be>
2013-12-04 20:59:02 +0100

    deel 1 examenvraag af

--- haskell4/kotnet.hs
+++ haskell4/kotnet.hs
@@ -1,12 +1,27 @@
+import Data.List
 -- Kotnet (ex-examenvraag)
 -- Deel 1
--- oorsprong :: Ord a => [[a]] -> [a]
+oorsprong :: Ord a => [[a]] -> [a]
+oorsprong = reconstruct []
 
+reconstruct :: Ord a => [a] -> [[a]] -> [a]
+reconstruct part [] = part
+reconstruct part list =
+    reconstruct (part ++ [nextel]) (verwijder nextel list)
+        where nextel = head $ sort $ zekervoor list
+
+verwijder :: Eq a => a -> [[a]] -> [[a]]
+verwijder el [] = []
+verwijder el list = filter (/= []) (map (filter (/= el)) list)
 
 zekervoor :: Eq a => [[a]] -> [a]
 zekervoor [] = []
 zekervoor list =
-    foldl1 (++) (map (zekervoor' list) list)
+    foldl1 (\list1 list2 -> if list2 == [] || elem (head list2) list1
+                                then list1
+                                else list1 ++ list2)
+            (map (zekervoor' list) list)
+
 zekervoor' :: Eq a => [[a]] -> [a] -> [a]
 zekervoor' list [] = []
 zekervoor' list (x:xs)
@@ -17,3 +32,6 @@ nietintails :: Eq a => [[a]] -> a -> Bool
 nietintails [] x = True
 nietintails listoflists x =
    not (foldr1 (||) (map (elem x) (map tail listoflists)))
+
+-- gegeven
+data GJ = Za | Zo | Ma | Di deriving (Eq,Ord,Show)
