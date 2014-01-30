b01d9808dabcea25dd7ea04a033ec56343b7fa9a
Toon Nolten <toon.nolten@student.kuleuven.be>
2014-01-30 23:56:14 +0100

    tweede deel prolog examen; geen idee wat er nog fout is palindroom([r,e,d,e]... werkt palindroom([r,e,d,e,n]... niet

--- prologEx/palindromen.pl
+++ prologEx/palindromen.pl
@@ -20,6 +20,7 @@ sequentie(Glyph, Len, [Glyph|Palindroom]) :-
     sequentie(Glyph, NewLen, Palindroom).
 
 algelijk([],0,[]).
+algelijk([X],0,[X]).
 algelijk(Woord, Kost, Palindroom) :-
     length(Woord, Len),
     setof(Count-Letter,
@@ -31,3 +32,68 @@ algelijk(Woord, Kost, Palindroom) :-
     sequentie(PalLetter, Len, Palindroom).
     
 % deel 2    
+groep_pos(Kars, PosGroepen) :-
+    length(Kars, Len),
+    findall([Pos-Kar|Groep],
+            (nth1(Index, Kars, Pos-Kar),
+            Index =< ceiling(Len/2),
+            findall(P-K,
+                    (member(P-K, Kars),
+                    Pos + P =:= Len + 1,
+                    Pos < P),
+                    Groep)),
+            PosGroepen).
+
+groep_kar([], []).
+groep_kar([Groep|Groepen], PalGroepen) :-
+    setof(G,
+            P1^P2^Kar^(member(G, Groepen),
+            member(P1-Kar, Groep),
+            member(P2-Kar, G)),
+            PGDeel),
+    !,
+    flatten([Groep|PGDeel], PalGroepDeel),
+    findall(G,
+            (member(G, Groepen),
+            \+ (member(G, PGDeel))),
+            RestGroepen),
+    groep_kar([PalGroepDeel|RestGroepen], PalGroepen).
+groep_kar([Groep|Groepen], [Groep|PalGroepen]) :-
+    groep_kar(Groepen, PalGroepen).
+
+groepen(Kars, PalGroepen) :-
+    groep_pos(Kars, PosGroepen),
+    groep_kar(PosGroepen, PalGroepen).
+
+splits(Paren, Indices, Kars) :-
+    sort(Paren, Sorted),
+    pairs_keys_values(Sorted, Indices, Kars).
+
+onsplits(Paren, Indices, Kars) :-
+    pairs_keys_values(Paren, Indices, Kars).
+
+combineer(Delen, Geheel) :-
+    pairs_keys_values(Delen, Pals, _),
+    flatten(Pals, FPals),
+    sort(FPals, Sorted),
+    pairs_keys_values(Sorted, _,Geheel).
+
+som([], Acc, Acc).
+som([X|R], Acc, Kost) :-
+    NAcc is Acc + X,
+    som(R, NAcc, Kost).
+kosten_som(Delen, Kost) :-
+    pairs_keys_values(Delen, _, Kosten),
+    som(Kosten, 0, Kost).
+
+palindroom(Woord, Kost, Palindroom) :-
+    findall(Pos-Kar, nth1(Pos, Woord, Kar), Kars),
+    groepen(Kars, PalGroepen),
+    findall(PalGroep-Kost,
+            (member(X, PalGroepen),
+            splits(X, Indices, Woord),
+            algelijk(Woord, Kost, Pal),
+            onsplits(PalGroep, Indices, Pal)),
+            PalDelen),
+    kosten_som(PalDelen, Kost),
+    combineer(PalDelen, Palindroom).
